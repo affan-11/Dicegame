5bf1e9e9798b3911d2dee3357011ebe5035addec
affan-11 <affanarif4@gmail.com>
2022-05-09 14:28:12 +0100

    working single-player dicegame

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ function refreshBtn() {
     console.log("refresh")
 }
 
-
+let player1total = 0
 
 function roll() {
     const firstRandomNum = Math.floor(Math.random()*6)+1;
     const firstDiceImage = 'images/dice' + firstRandomNum + '.png';
     document.querySelectorAll('img')[0].setAttribute('src', firstDiceImage)
 
-    let player1total = 0
+    
 
     player1total += firstRandomNum
     
@@ -19,7 +19,7 @@ function roll() {
     document.getElementById("score").innerHTML = player1total;
     if (firstRandomNum === 1) {
         document.querySelector('h1').innerHTML = 'You lose!';
-    } else if ( firstRandomNum >= 20){
+    } else if ( player1total >= 20){
         document.querySelector('h1').innerHTML = 'You win!';
     } else {
         document.querySelector('h1').innerHTML = 'Keep rolling!';
